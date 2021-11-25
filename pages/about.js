875e0299c9923436ca06577c939dc1596e88e81b
mentari <erizkimentari@gmail.com>
2021-11-25 11:01:25 +0700

    Update

--- pages/about.js
+++ pages/about.js
@@ -15,10 +15,7 @@ function About(){
         <div className="container">
           <div className="text-wrapper w-full">
             <h1 className="title">About Me</h1>
-            <p className="description">Hello, my name is Mentari E Rizki. <br></br> Currently, I&apos;m studying at BINUS University and taking the Computer Science major. Looking back in my first job, I started it as an internship with a junior backend developer role using the PHP programming language, the CodeIgniter framework, and used PostgreSQL as the database. Afterward, I learn about the MERN stack technology using MongoDB, ExpressJS, React, and NodeJS. Now, I am learning Go programming language as well as MongoDB and eagerly deepening my knowledge in writing clean, secure, test-driven, modular, and easy maintenance codes.</p>
-            <div>
-              <h1 className="subtitle">Actively used</h1>
-            </div>
+            <p className="paragraph">Hello, my name is Mentari E Rizki. <br></br> Currently, I&apos;m studying at BINUS University and taking the Computer Science major. Looking back in my first job, I started it with an internship as a junior backend developer role used the PHP programming language, the CodeIgniter framework, and used PostgreSQL as the database. Afterward, I learn about the MERN stack technology used MongoDB, ExpressJS, React, and NodeJS. Now, I&apos;m learning Go programming language as well as MongoDB and eagerly deepening my knowledge in writing clean, secure, test-driven, modular, and easy maintenance codes.</p>
           </div>
         </div>
       </section>
